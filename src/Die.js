a746677228ee3f98cffc8510027ef6c7eb20241e
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-24 12:39:31 -0600

    Setting up rolling boolean

--- src/Die.js
+++ src/Die.js
@@ -3,11 +3,6 @@ import React, {useEffect} from 'react'
 
 const Die = ({number}) => {
 
-    useEffect(() => {
-        const timer = setTimeout(() => document.querySelector('.Die--image').classList.add('jiggle'), 1000);
-        clearTimeout(timer);
-      }, [])
-
     return ( 
         <>
         <i className={`Die--image fas fa-dice-${number} fa-6x`}></i>
--- src/RollDice.js
+++ src/RollDice.js
@@ -6,7 +6,8 @@ const RollDice = () => {
   const dieArray = ["one", "two", "three", "four", "five", "six"];
 
   const [dieOne, setDieOne] = useState("");
-  const [dieTwo, setDieTwo] = useState("");  
+  const [dieTwo, setDieTwo] = useState("");
+  const [rolling, setRolling] = useState(false);  
 
   const handleRoll = () => {
     const roll = function () {
@@ -15,14 +16,15 @@ const RollDice = () => {
     };
     setDieOne(roll());
     setDieTwo(roll());
+    setRolling(true)
   };
 
   return (
     <>
       <div className="RollDice">
         <div className="RollDice--Die">
-          <Die number={dieOne} />
-          <Die number={dieTwo} />
+          <Die number={dieOne} rolling={rolling} />
+          <Die number={dieTwo} rolling={rolling}/>
         </div>
         <button onClick={handleRoll}>Roll Die!</button>
       </div>
